3d37bcb7b0e2f4a19246ff3bdfab90138dafb71d
kirankalsi <kiran11kalsi@gmail.com>
2020-10-27 15:15:27 +0000

    simplify tests

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,14 +1,8 @@
 import pytest
 from programs import factorial
 
-def test_fact_two():
-    assert factorial.fact(2) == 2
-
-def test_fact_three():
-    assert factorial.fact(3) == 6
-
-def test_fact_four():
-    assert factorial.fact(4) == 24
+def test_fact_zero():
+    assert factorial.fact(0) == 1
 
 def test_fact_five():
     assert factorial.fact(5) == 120
