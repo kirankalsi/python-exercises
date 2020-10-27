f1fb48dfe6bc75b112be54ca426c3dd8bf1053ab
kirankalsi <kiran11kalsi@gmail.com>
2020-10-27 14:03:39 +0000

    test_grade_boundary function testing

--- tests/test_functions_ex.py
+++ tests/test_functions_ex.py
@@ -4,7 +4,17 @@ from programs import functions_ex
 def test_grade_calculator_full_marks():
     assert functions_ex.grade_calculator(25, 50, 100) == 100
 
-def test_grade_calculator_full_marks():
+def test_grade_calculator_invalid_input():
     assert functions_ex.grade_calculator(55, 50, 60) == False
+
+
+def test_grade_boundary_A():
+    assert functions_ex.grade_boundary(88) == 'A'
+
+def test_grade_boundary_C():
+    assert functions_ex.grade_boundary(65) == 'C'
+
+def test_grade_boundary_A():
+    assert functions_ex.grade_boundary(37) == 'F'
 
 #pytest -s
