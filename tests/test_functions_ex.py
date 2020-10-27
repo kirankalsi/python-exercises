0ef1c50c2524bd37ce39d6bd87eaf7d743e41605
kirankalsi <kiran11kalsi@gmail.com>
2020-10-27 15:50:06 +0000

    testing coverage 93%

--- tests/test_functions_ex.py
+++ tests/test_functions_ex.py
@@ -7,14 +7,25 @@ def test_grade_calculator_full_marks():
 def test_grade_calculator_invalid_input():
     assert functions_ex.grade_calculator(55, 50, 60) == False
 
+def test_grade_calculator_invalid_input():
+    assert functions_ex.grade_calculator(-5, 50, 60) == False
+
+def test_grade_boundary_Astar():
+    assert functions_ex.grade_boundary(90) == 'A*'
 
 def test_grade_boundary_A():
     assert functions_ex.grade_boundary(88) == 'A'
 
+def test_grade_boundary_B():
+    assert functions_ex.grade_boundary(72) == 'B'
+
 def test_grade_boundary_C():
     assert functions_ex.grade_boundary(65) == 'C'
 
-def test_grade_boundary_A():
+def test_grade_boundary_D():
+    assert functions_ex.grade_boundary(56) == 'D'
+
+def test_grade_boundary_F():
     assert functions_ex.grade_boundary(37) == 'F'
 
 #pytest -s
