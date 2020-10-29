ef09ea42057439b89128fc3171891d6729e2237d
kirankalsi <kiran11kalsi@gmail.com>
2020-10-29 15:49:34 +0000

    update best conjugation task

--- programs/best_conjugation.py
+++ programs/best_conjugation.py
@@ -5,10 +5,11 @@ def best_conjugation(string):
     total = len(subwords)
     words_list = [word for line in f_contents for word in line.split()]
     for word in words_list:
-        if word in string:
+        if word in string and len(word) >= 2:
             subwords.append(word)
-    return subwords
-    f_contents.close() 
+    msg = f"{len(subwords)} Subwords found which are: {subwords}"
+    f_contents.close()
+    return msg 
 
 print(best_conjugation('awesomeness'))
 
